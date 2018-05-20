d0aa2e5cfec12dc9c4b7a67f856ebfd46277415a
volfovskaya <anastasya-wolff@yandex.ru>
2018-05-21 00:15:16 +0300

    Added class Course

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -5,9 +5,9 @@ public class Employee {
     private String employee_birth;
     private String employee_position;
 
-    private String employee_pc5;
-    private String employee_pc6;
-    private String employee_pc15;
+    private int employee_pc5;
+    private int employee_pc6;
+    private int employee_pc15;
 
     public int getEmployee_id() {
         return employee_id;
@@ -25,15 +25,15 @@ public class Employee {
         return employee_position;
     }
 
-    public String getEmployee_pc5() {
+    public int getEmployee_pc5() {
         return employee_pc5;
     }
 
-    public String getEmployee_pc6() {
+    public int getEmployee_pc6() {
         return employee_pc6;
     }
 
-    public String getEmployee_pc15() {
+    public int getEmployee_pc15() {
         return employee_pc15;
     }
 
@@ -53,19 +53,29 @@ public class Employee {
         this.employee_position = employee_position;
     }
 
-    public void setEmployee_pc5(String employee_pc5) {
+    public void setEmployee_pc5(int employee_pc5) {
         this.employee_pc5 = employee_pc5;
     }
 
-    public void setEmployee_pc6(String employee_pc6) {
+    public void setEmployee_pc6(int employee_pc6) {
         this.employee_pc6 = employee_pc6;
     }
 
-    public void setEmployee_pc15(String employee_pc15) {
+    public void setEmployee_pc15(int employee_pc15) {
         this.employee_pc15 = employee_pc15;
     }
 
-    public Employee(int employee_id, String employee_name, String employee_birth, String employee_position, String employee_pc5, String employee_pc6, String employee_pc15) {
+    public Employee() {
+        this.employee_id = -1;
+        this.employee_name = "";
+        this.employee_birth = "";
+        this.employee_position = "";
+        this.employee_pc5 = -1;
+        this.employee_pc6 = -1;
+        this.employee_pc15 = -1;
+    }
+
+    public Employee(int employee_id, String employee_name, String employee_birth, String employee_position, int employee_pc5, int employee_pc6, int employee_pc15) {
         this.employee_id = employee_id;
         this.employee_name = employee_name;
         this.employee_birth = employee_birth;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,42 @@
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        ArrayList<Employee> arrayListEmployee = new ArrayList<>();
+
+
+
         DBWorker dbWorker = new DBWorker();
 
         try {
             Statement statement = dbWorker.getConnection().createStatement();
-            ResultSet res = statement.executeQuery("SELECT * FROM employee_start");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_start");
 
-            while (res.next()) {
-                int id = res.getInt("employee_id");
+
+            arrayListEmployee.add(new Employee());
+            arrayListEmployee.add(new Employee());
+            int i = 0;
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("employee_id");
                 System.out.println(id);
-                String str = res.getString("employee_name");
+
+                String str = resultSet.getString("employee_name");
                 System.out.println(str);
+
+                Date date = resultSet.getDate("employee_birth");
+                System.out.println(date);
+
+
             }
+
+
 
 
 
