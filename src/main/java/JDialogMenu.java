eee3da6c09b27788a213b33ecee7d37bcc67538c
volfovskaya <anastasya-wolff@yandex.ru>
2018-06-09 03:24:24 +0300

    add button

--- src/main/java/JDialogMenu.java
+++ src/main/java/JDialogMenu.java
@@ -1,12 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class JDialogMenu extends JFrame {
     private static final Dimension DISPLAY_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
     private static final Dimension FRAME_SIZE = new Dimension(400, 400);
     private static final Dimension PANEL_SIZE = new Dimension(80, 40);
     private static final Dimension TEXT_FIELD_SIZE = new Dimension(70, 20);
+    private static final Dimension BUTTON_SIZE = new Dimension(170, 40);
 
+    private int budget = 0;
 
 
     public JDialogMenu() {
@@ -18,35 +22,63 @@ public class JDialogMenu extends JFrame {
         this.setTitle("АСПРПК");
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JPanel jBudgetPanel = new JPanel();
-        jBudgetPanel.setPreferredSize(PANEL_SIZE);
+        JPanel jPanelWelcome = new JPanel();
+        jPanelWelcome.setPreferredSize(new Dimension(80, 80));
 
+        JPanel jPanelBudgetTxt = new JPanel();
+        jPanelBudgetTxt.setPreferredSize(PANEL_SIZE);
 
+        JPanel jPanelAcceptLimitsBt = new JPanel();
+        jPanelAcceptLimitsBt.setPreferredSize(PANEL_SIZE);
+
+        JTextArea jTextFieldWelcome = new JTextArea("Добро пожаловать в АСПРПК!\n" +
+                "Выберите ограничения на бюджет, количество обучаемых на одном \n" +
+                "курсе и общее количество обучаемых");
+        jPanelWelcome.add(jTextFieldWelcome, BorderLayout.CENTER);
+        this.add(jPanelWelcome, BorderLayout.NORTH);
 
         JTextField jBudget = new JTextField("250");
 
         jBudget.setVisible(true);
         jBudget.setAlignmentX(FRAME_SIZE.width / 2);
         jBudget.setAlignmentY(FRAME_SIZE.height / 2);
+        jBudget.setMinimumSize(TEXT_FIELD_SIZE);
+        jBudget.setMaximumSize(TEXT_FIELD_SIZE);
         jBudget.setPreferredSize(TEXT_FIELD_SIZE);
 
 
-
-        jBudget.createToolTip();
         jBudget.setToolTipText("Введите ограничение на бюджет");
+        jBudget.createToolTip();
 
-        jBudgetPanel.add(jBudget, BorderLayout.CENTER);
+        jPanelBudgetTxt.add(jBudget, BorderLayout.CENTER);
+        this.getContentPane().add(jPanelBudgetTxt, BorderLayout.CENTER);
 
-        this.getContentPane().add(jBudgetPanel, BorderLayout.NORTH);
+        JButton jButtonAcceptLimits = new JButton("Принять ограничения");
+        jButtonAcceptLimits.setPreferredSize(BUTTON_SIZE);
+
+        jPanelAcceptLimitsBt.add(jButtonAcceptLimits, BorderLayout.CENTER);
+        this.getContentPane().add(jPanelAcceptLimitsBt, BorderLayout.AFTER_LAST_LINE);
+
+        budget = Integer.parseInt(jBudget.getText());
+
+        jButtonAcceptLimits.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    budget = Integer.parseInt(jBudget.getText());
+                } catch (NumberFormatException e1) {
+
+                    JOptionPane.showMessageDialog(null, "Ошибка");
+                }
 
 
+                System.out.println("Получен бюджет: " + budget);
 
-
-
+            }
+        });
 
 
         this.setVisible(true);
-
 
 
     }
